029f35e176bf8a0707861761e4b416b7b0cdc81c
ManishaBose <manishabose.2001@gmail.com>
2024-04-07 20:12:55 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Simon Game
+
+## Description
+
+Simon Game is a memory game that allows users to test and improve their memory. It is built using HTML, CSS, JavaScript, and jQuery, and is hosted on Cloudflare.
+
+## How to Play
+
+The game presents a series of tones and lights and requires a user to repeat the sequence. If the user succeeds, the series becomes progressively longer and more complex. Once the user fails or the time limit is reached, the game is over.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- jQuery
+- Hosted on Cloudflare
+
+## Installation
+
+Since the game is hosted on Cloudflare, no installation is necessary. Simply navigate to the game's URL in your web browser to start playing.
+
+## Play live game
+
+[Play live game here](www.)
---
+++ index.js
@@ -0,0 +1,71 @@
+var userClickedPattern = [];
+var gamePattern = [];
+var buttonColors = ["red", "blue", "green", "yellow"];
+var started = false;
+var level = 0;
+
+$(document).keypress(() => {
+  if (!started) {
+    $("h1").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("h1").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColor);
+}
+
+function playSound(name) {
+  var audio = new Audio("./Assets/sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(() => {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(() => {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("h1").text("Game Over, Press Any Key to Restart");
+    setTimeout(() => {
+      $("body").removeClass("game-over");
+    }, 200);
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
